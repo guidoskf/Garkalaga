fda80a8357fe26d1b9821a226d83308c511e27dd
guidoskf <70548681+guidoskf@users.noreply.github.com>
2023-01-06 09:14:41 -0300

    Empecé a implementar la touchscreen

---
+++ Assets/_Scripts/TouchManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TouchManager : MonoBehaviour
+{
+    private PlayerInput playerInput;
+
+    private InputAction touchPositionAction;
+    private InputAction touchPressAction;
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        touchPressAction = playerInput.actions.FindAction("TouchPress");
+        touchPositionAction = playerInput.actions.FindAction("TouchPosition");
+    }
+}
