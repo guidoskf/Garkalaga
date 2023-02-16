4fd1179904f7c171ee37387d739a80445aef3d5f
Guido Skerianz <70548681+guidoskf@users.noreply.github.com>
2023-02-16 10:47:04 -0300

    La nave dispara y se mueve bien
    Empecé a implementar el fondo

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] GameObject firePoint;
+    private bool reload = false;
+    [SerializeField] float reloading = 0.2f;
+    [SerializeField] float bulletForce = 25f;
+    [SerializeField] GameObject bulletPrefab;
     /*
     private Touch touch;
     [SerializeField] float speedModifier = 0.01f;
@@ -30,20 +35,45 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         if (Input.touchCount > 0)
         {
+            if(reload == false)
+            {
+                Shoot();
+                reload = true;
+            }
+
             Touch touch = Input.GetTouch(0);
             touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
             touchPosition.z = 0;
             direction = (touchPosition - transform.position);
-            rb.velocity = new Vector2(direction.x, direction.y) * moveSpeed;
+            rb.velocity = new Vector2(direction.x, direction.y) * moveSpeed * Time.deltaTime;
 
             if (touch.phase == TouchPhase.Ended)
             {
                 rb.velocity = Vector2.zero;
             }
         }
+        else
+        {
+            rb.velocity = new Vector2(0, 0);
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, new Vector3(firePoint.transform.position.x, firePoint.transform.position.y, firePoint.transform.position.z), firePoint.transform.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.transform.up * bulletForce, ForceMode2D.Impulse);
+        StartCoroutine(Reloading());
+    }
+
+    IEnumerator Reloading()
+    {
+        yield return new WaitForSeconds(reloading);
+
+        reload = false;
     }
 }
